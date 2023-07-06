375e2460e85bc38d9946fe7c6c5debe7ab436d5f
seriouslygooddesign <129963693+seriouslygooddesign@users.noreply.github.com>
2023-07-06 10:22:26 +0300

    add global variables from inc/whats-on.php

--- inc/variables.php
+++ inc/variables.php
@@ -3,3 +3,8 @@ define('FOOTER_SIDEBAR_QUANTITY', 4);
 define('CONTENT_BLOCK_CLASS', 'content-block');
 define('CONTENT_BLOCK_CONTENT', CONTENT_BLOCK_CLASS . '__content');
 define('CONTENT_BLOCK_MODIFIER', CONTENT_BLOCK_CLASS . '--');
+
+define('WHATS_ON_POST_TYPE_NAME', 'whats_on');
+define('WHATS_ON_URL_PREFIX', 'whats-on');
+define('WHATS_ON_TEMPLATE_NAME', 'single-' . WHATS_ON_POST_TYPE_NAME . '.php');
+define('WHATS_ON_QUERY_VAR', 'whats_on_title');
