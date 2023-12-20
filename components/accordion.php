e64e0ed52b848321546af194b5cc87250b0c92cc
seriouslygooddesign <129963693+seriouslygooddesign@users.noreply.github.com>
2023-12-20 13:38:09 +0200

    Update accordion.php faq schema

--- components/accordion.php
+++ components/accordion.php
@@ -7,44 +7,38 @@ extract(wp_parse_args($args, [
     'total' => 0,
     'current' => 1, // Start from 1
     'id' => wp_unique_id(),
-    'faq_scheme' => true,
+    'faq_schema' => false,
 ]));
 
+if ($total < 1) return;
 
-$faq_scheme_open = $faq_scheme_item = $faq_scheme_title = $faq_scheme_answer = '';
+$faq_schema_page = $faq_schema_question = $faq_schema_name = $faq_schema_answer = $faq_schema_text = '';
 
-if ($faq_scheme) {
-    $faq_scheme_open = " itemscope='' itemtype='https://schema.org/FAQPage'";
-    $faq_scheme_item = " itemscope='' itemprop='mainEntity' itemtype='https://schema.org/Question'";
-    $faq_scheme_title = " itemprop='name'";
-    $faq_scheme_answer = " itemscope='' itemprop='acceptedAnswer' itemtype='https://schema.org/Answer'";
+if ($faq_schema) {
+    $faq_schema_page = "itemscope itemtype='https://schema.org/FAQPage'";
+    $faq_schema_question = "itemscope itemprop='mainEntity' itemtype='https://schema.org/Question'";
+    $faq_schema_name = "itemprop='name'";
+    $faq_schema_answer = "itemscope itemprop='acceptedAnswer' itemtype='https://schema.org/Answer'";
+    $faq_schema_text = "itemprop='text'";
 }
 
-if ($total > 0) : ?>
-    <?php
-    $accordion_id = "accordion-$id";
-    $accordion_title_id = "accordion-title-$id";
-    $label_class = $label_class ? "class='$label_class'" : null;
+$accordion_id = "accordion-$id";
+$accordion_title_id = "accordion-title-$id";
+$label_class = $label_class ? "class='$label_class'" : null;
 
-    //open parent div
-    if ($current === 1) {
-        echo "<div class='accordion'$faq_scheme_open>";
-    }
-    ?>
-    <div class="accordion__item" data-animate<?= $faq_scheme_item; ?>>
-        <h3 class="accordion__title h5" <?= $faq_scheme_title; ?>>
-            <button type="button" id="<?= $accordion_title_id; ?>" aria-expanded="false" aria-controls="<?= $accordion_id; ?>" class="accordion__button button">
-                <span <?= $label_class; ?>><?= esc_html($title); ?></span>
-                <?php get_template_part('components/site-icon', null, ['icon' => 'chevron', 'class' => 'accordion__icon']); ?>
-            </button>
-        </h3>
-        <div id="<?= $accordion_id; ?>" class="accordion__content <?= $content_class; ?> element-collapse" aria-labelledby="<?= $accordion_title_id; ?>" role="region" <?= $faq_scheme_answer; ?>>
-            <?= $faq_scheme ? get_core_accordion_first_p($text) : $text; ?>
+if ($current === 1) echo "<div class='accordion' $faq_schema_page>"; //open parent div
+?>
+<div class="accordion__item" data-animate <?= $faq_schema_question; ?>>
+    <h3 class="accordion__title h5">
+        <button type="button" id=" <?= $accordion_title_id; ?>" aria-expanded="false" aria-controls="<?= $accordion_id; ?>" class="accordion__button button">
+            <span <?= $label_class; ?> <?= $faq_schema_name; ?>><?= esc_html($title); ?></span>
+            <?php get_template_part('components/site-icon', null, ['icon' => 'chevron', 'class' => 'accordion__icon']); ?>
+        </button>
+    </h3>
+    <div id="<?= $accordion_id; ?>" class="accordion__content <?= $content_class; ?> element-collapse" aria-labelledby="<?= $accordion_title_id; ?>" role="region" <?= $faq_schema_answer; ?>>
+        <div <?= $faq_schema_text; ?>>
+            <?= $text;  ?>
         </div>
     </div>
-<?php
-    //close parent div
-    if ($current === $total) {
-        echo "</div>";
-    }
-endif;
+</div>
+<?php if ($current === $total) echo "</div>"; //close parent div
