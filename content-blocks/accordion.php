288f734d20a548ec51550edfb1c720c1f12cfeb1
seriouslygooddesign <129963693+seriouslygooddesign@users.noreply.github.com>
2023-12-20 13:40:06 +0200

    Update accordion.php

--- content-blocks/accordion.php
+++ content-blocks/accordion.php
@@ -5,12 +5,14 @@ $block_args = [
 get_template_part('components/block', 'start', $block_args);
 get_template_part('components/block', 'header');
 $total = count(get_sub_field('accordion'));
+$faq_schema = get_sub_field('faq_schema');
 if (have_rows('accordion')) {
     while (have_rows('accordion')) {
         the_row();
         $accordion_args = [
             'total' => $total,
-            'current' => get_row_index()
+            'current' => get_row_index(),
+            'faq_schema' => $faq_schema
         ];
         get_template_part('components/accordion', null, $accordion_args);
     }
